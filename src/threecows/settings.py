c6f03460bcec935dde9dd21e04765beb0497003b
nickho6617 <nickholme1@gmail.com>
2019-11-23 16:29:22 +0800

    added customer user model and some tests

--- src/threecows/settings.py
+++ src/threecows/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'core.User'
