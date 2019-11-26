348693efbaee00b30d059b502abf68c556f7805c
nickho6617 <nickholme1@gmail.com>
2019-11-26 22:55:14 +0800

    added tags model and api auth tests

--- src/threecows/settings.py
+++ src/threecows/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # custom
     'core',
     'user',
+    'cattle',
 ]
 
 MIDDLEWARE = [
--- src/threecows/urls.py
+++ src/threecows/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/user/', include('user.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/cattle/', include('cattle.urls')),
 ]
